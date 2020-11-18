b8d01842701fc5a166fbcef936eeecd1460abd44
Victor Benichoux <vbenichoux@csod.com>
2020-11-18 17:53:00 +0100

    vyz: add reloading to report CLI

--- bin/vyz.py
+++ bin/vyz.py
@@ -1,24 +1,51 @@
 #!/usr/bin/env python3
 
-import typer
+import copy
 import os
 import subprocess
 import sys
-import copy
+import time
+
+import typer
+import watchdog.events
+import watchdog.observers
 
 realpath = os.path.realpath(__file__)
 dir_realpath = os.path.dirname(os.path.dirname(realpath))
 sys.path.append(dir_realpath)
 
 
-def report(report_fn: str):
+class ReportFileChangedHandler(watchdog.events.PatternMatchingEventHandler):
+    def on_modified(self, event):
+        super(ReportFileChangedHandler, self).on_modified(event)
+        print(f"Detected file change in {event.src_path}...")
+        run_report_file(event.src_path)
+
+
+def run_report_file(fn):
     env = copy.copy(os.environ)
     if "PYTHONPATH" in env:
         env["PYTHONPATH"] += ":" + dir_realpath
     else:
         env["PYTHONPATH"] = dir_realpath
+    subprocess.call([sys.executable, fn], env=env)
 
-    subprocess.call([sys.executable, os.path.realpath(report_fn)], env=env)
+
+def report(report_fn: str, reload: bool = False):
+    fn = os.path.realpath(report_fn)
+    if not reload:
+        run_report_file(fn)
+    else:
+        event_handler = ReportFileChangedHandler(patterns=[fn])
+        observer = watchdog.observers.Observer()
+        observer.schedule(event_handler, os.path.dirname(fn))
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
 
 if __name__ == "__main__":
