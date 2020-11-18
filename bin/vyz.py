9c03abd9ce656c8f4093568d664ae8a148cbb272
Victor Benichoux <vbenichoux@csod.com>
2020-11-18 20:47:23 +0100

    vyz/bin: add dev mode for the CLI to reload on src file changes

--- bin/vyz.py
+++ bin/vyz.py
@@ -16,10 +16,14 @@ sys.path.append(dir_realpath)
 
 
 class ReportFileChangedHandler(watchdog.events.PatternMatchingEventHandler):
+    def __init__(self, target_report_fn, **kwargs):
+        self.target_report_fn = target_report_fn
+        super().__init__(**kwargs)
+
     def on_modified(self, event):
         super(ReportFileChangedHandler, self).on_modified(event)
         print(f"Detected file change in {event.src_path}...")
-        run_report_file(event.src_path)
+        run_report_file(self.target_report_fn)
 
 
 def run_report_file(fn):
@@ -31,14 +35,17 @@ def run_report_file(fn):
     subprocess.call([sys.executable, fn], env=env)
 
 
-def report(report_fn: str, reload: bool = False):
+def report(report_fn: str, reload: bool = False, dev: bool = False):
     fn = os.path.realpath(report_fn)
-    if not reload:
-        run_report_file(fn)
-    else:
-        event_handler = ReportFileChangedHandler(patterns=[fn])
+    run_report_file(fn)
+    if reload:
         observer = watchdog.observers.Observer()
+        event_handler = ReportFileChangedHandler(fn, patterns=[fn])
         observer.schedule(event_handler, os.path.dirname(fn))
+        if dev:
+            vyz_src_path = os.path.join(dir_realpath, "vyz")
+            dev_event_handler = ReportFileChangedHandler(fn, patterns=["*.js", "*.py"])
+            observer.schedule(dev_event_handler, vyz_src_path, recursive=True)
         observer.start()
         try:
             while True:
