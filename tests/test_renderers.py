776df95ab278bc937b4732244179b3d3f8ef8a23
Victor Benichoux <vbenichoux@csod.com>
2020-11-20 00:18:19 +0100

    tests/renderers: use fixture

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -3,32 +3,37 @@ import tempfile
 import matplotlib.pyplot as plt
 import pytest
 
-from vyz.report import Report
-
+from vyz.renderers import FallbackContentConfiguration
 from vyz.renderers.matplotlib import MatplotlibContentConfiguration
 from vyz.renderers.text import TextContentConfiguration
-from vyz.renderers import FallbackContentConfiguration
+from vyz.report import Report
 
 
 class OddContent:
     pass
 
 
-@pytest.mark.parametrize(
-    "content, config_type",
-    [
-        ("Bonjour", TextContentConfiguration),
-        (plt.figure(), MatplotlibContentConfiguration),
-        (OddContent(), FallbackContentConfiguration),
-    ],
-)
-def test_renderers(content, config_type):
+@pytest.fixture(scope="module", params=["str", "matplotlib", "odd"])
+def content(request):
+
+    if request.param == "str":
+        yield ("Bonjour", TextContentConfiguration)
+    if request.param == "matplotlib":
+        f = plt.figure()
+        yield (f, MatplotlibContentConfiguration)
+        f.clf()
+        del f
+    if request.param == "odd":
+        yield (OddContent(), FallbackContentConfiguration)
+
+
+def test_renderers(content):
     r = Report()
 
     @r.widget()
     def something():
-        return content
+        return content[0]
 
-    assert isinstance(r.get_configuration().widgets[0].content, config_type)
+    assert isinstance(r.get_configuration().widgets[0].content, content[1])
     with tempfile.TemporaryDirectory() as tmpdir:
         r.render(tmpdir)
