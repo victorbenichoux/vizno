3271320e629e28c37d9ac9c8abce49ea451cd023
Victor Benichoux <vbenichoux@csod.com>
2020-11-25 16:28:23 +0100

    examples: fix API example

--- examples/api.py
+++ examples/api.py
@@ -1,11 +1,15 @@
 from vizno.api import ViznoApp
 from vizno.report import Report
+from typing import Optional
 
 app = ViznoApp()
 
 
-@app.report("/something")
-def f(value: str):
+@app.report("/")
+def f(value: Optional[str] = None):
     r = Report()
-    r.widget(value, name="Result here")
+    if value is None:
+        r.text(f"No value provided, try to add a value for example like [this](http://localhost:8000?value=\"with a value\")", name="Result here")
+    else:
+        r.text(f"The value was {value}", name="Result here")
     return r.get_configuration()
