249049029c094947b2ac80d7903d1ee289007095
Victor Benichoux <vbenichoux@csod.com>
2020-11-25 16:31:23 +0100

    examples: improve API example

--- examples/api.py
+++ examples/api.py
@@ -8,8 +8,16 @@ app = ViznoApp()
 @app.report("/")
 def f(value: Optional[str] = None):
     r = Report()
+    r.text("# Report served with the API")
     if value is None:
-        r.text(f"No value provided, try to add a value for example like [this](http://localhost:8000?value=\"with a value\")", name="Result here")
+        r.text(f"""
+No value provided.
+
+
+Try to add a value in the search string.
+
+For example [like this](http://localhost:8000?value="with a value")
+""", name="Result here")
     else:
         r.text(f"The value was {value}", name="Result here")
     return r.get_configuration()
