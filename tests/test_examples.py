5cdca39fc2840a4e76332fc8e35af8bed488d8e1
Victor Benichoux <vbenichoux@csod.com>
2020-11-26 22:39:04 +0100

    examples: use cli.py in tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,14 +2,16 @@ import copy
 import os
 import subprocess
 import sys
+import tempfile
 
 import pytest
 
 from tests import ROOT_DIR
 
+EXAMPLES_DIR = os.path.join(ROOT_DIR, "examples", "reports")
 EXAMPLES = [
-    os.path.join(ROOT_DIR, "examples", fn)
-    for fn in os.listdir(os.path.join(ROOT_DIR, "examples"))
+    os.path.join(EXAMPLES_DIR, fn)
+    for fn in os.listdir(os.path.join(EXAMPLES_DIR))
     if fn.endswith(".py")
 ]
 
@@ -21,4 +23,18 @@ def test_examples(example_fn):
         env["PYTHONPATH"] += ":" + ROOT_DIR
     else:
         env["PYTHONPATH"] = ROOT_DIR
-    assert subprocess.call([sys.executable, example_fn], env=env) == 0
+    with tempfile.TemporaryDirectory() as tmpdir:
+        assert (
+            subprocess.call(
+                [sys.executable, "vizno/cli.py", example_fn, "--output-dir", tmpdir],
+                env=env,
+            )
+            == 0
+        )
+        assert set(os.listdir(tmpdir)) == {
+            "index.html",
+            "vizno.css",
+            "vizno-core.min.js",
+            "vz-ico.png",
+            "vizno-config.js",
+        }
