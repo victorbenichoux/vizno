1559a8cd6e8ed693173ddec01ff91ed5931f292e
Victor Benichoux <vbenichoux@csod.com>
2020-11-27 20:20:22 +0100

    cli: do not use watchdog, check mtime directly

--- vizno/cli.py
+++ vizno/cli.py
@@ -1,60 +1,16 @@
 import copy
 import importlib
 import os
-import queue
 import subprocess
 import sys
 import tempfile
+import time
 from contextlib import contextmanager
 
 import requests
 import typer
-import watchdog.events
-import watchdog.observers
 
 from vizno.report import Report  # noqa: E402
-
-
-class ReportFileChangedHandler(watchdog.events.PatternMatchingEventHandler):
-    def __init__(self, queue, **kwargs):
-        super().__init__(**kwargs)
-        self.queue = queue
-
-    def on_modified(self, event):
-        self.queue.put(True)
-
-
-class ObserverState:
-    observer: watchdog.observers.Observer
-    queue: queue.Queue
-    fn: str
-
-    def __init__(self, fn):
-        self.fn = fn
-        self.queue = queue.Queue()
-        self.observer = None
-
-    def start(self):
-        self.observer = watchdog.observers.Observer()
-        self.queue = queue.Queue()
-        event_handler = ReportFileChangedHandler(queue=self.queue, patterns=[self.fn])
-        self.observer.schedule(event_handler, os.path.dirname(self.fn))
-        self.observer.start()
-
-    def stop(self):
-        if self.observer:
-            self.observer.stop()
-            self.observer.join()
-            self.observer = None
-
-
-@contextmanager
-def safe_observer_bypass(obs):
-    obs.stop()
-    try:
-        yield
-    finally:
-        obs.start()
 
 
 @contextmanager
@@ -100,19 +56,15 @@ def render(report_fn: str, output_dir: str = ".", reload: bool = False):
     fn = os.path.realpath(report_fn)
     render_from_file(fn, report_fn)
     if reload:
-        obs = ObserverState(fn)
-        obs.start()
+        current_mtime = os.stat(fn).st_mtime
         try:
             while True:
-                try:
-                    obs.queue.get(timeout=1)
-                    with safe_observer_bypass(obs):
-                        render_from_file(fn, report_fn)
-                except queue.Empty:
-                    pass
+                time.sleep(0.1)
+                if os.stat(fn).st_mtime != current_mtime:
+                    current_mtime = os.stat(fn).st_mtime
+                    render_from_file(fn, report_fn)
         except KeyboardInterrupt:
             pass
-        obs.stop()
 
 
 @app.command()
@@ -121,23 +73,19 @@ def serve(report_fn: str):
     with tempfile.TemporaryDirectory() as tmpdir:
         fn = os.path.realpath(report_fn)
         render_from_file(fn, tmpdir)
-        obs = ObserverState(fn)
+        current_mtime = os.stat(fn).st_mtime
         env["SERVER_DIR"] = tmpdir
         proc = subprocess.Popen(["uvicorn", "vizno.server:app"], env=env)
-        obs.start()
         try:
             while True:
-                try:
-                    obs.queue.get(timeout=0.1)
-                    with safe_observer_bypass(obs):
-                        render_from_file(fn, tmpdir)
-                        requests.post("http://127.0.0.1:8000/update")
-                except queue.Empty:
-                    pass
+                time.sleep(0.1)
+                if os.stat(fn).st_mtime != current_mtime:
+                    current_mtime = os.stat(fn).st_mtime
+                    render_from_file(fn, tmpdir)
+                    requests.post("http://127.0.0.1:8000/update")
         except KeyboardInterrupt:
             pass
         proc.terminate()
-        obs.stop()
 
 
 def main():
